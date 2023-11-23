c8cb7a4380c6cac13b4c84bd44b26d9bf7edb2cb
louie <hmedeiros@kokku.com.br>
2023-11-23 16:02:52 -0300

    base of gameloop structured on empty methods

--- TheHangMan/Program.cs
+++ TheHangMan/Program.cs
@@ -8,16 +8,46 @@ namespace TheHangMan
 {
     internal class Program
     {
+        WordProvider wordProvider = WordProvider.GetInstance();
+
+        private int MAX_MISTAKES_AVAILABLE = 5;
+        private string currentWord = "";
+
         static void Main(string[] args)
         {
-           WordProvider wordProvider = WordProvider.GetInstance();
-
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(wordProvider.GetNewWord() + "\n");
-            }
+            WriteScreenHeader();
+            StartMatch();
 
             Console.ReadLine();
+        }
+
+        static void WriteScreenHeader()
+        {
+            Console.WriteLine("--------------- The Hangman Tale ---------------\n");
+            Console.WriteLine("--- Guess the word before the man is hangged ---\n");
+        }
+
+        static void StartMatch()
+        {
+            //printar aqui a forca vazia
+            //escolher uma palavra e desenhar os traços
+            //chamar gameloop
+            MatchLoop();
+        }
+
+        static void MatchLoop()
+        {
+            int currentMistakesMade = 0;
+            List<char> wordsTried = new List<char>();
+            //enquanto a palavra estiver sendo descoberta mantém o loop
+
+            ClearMatchScreen();
+        }
+
+        static void ClearMatchScreen()
+        {
+            Console.Clear();
+            WriteScreenHeader();
         }
     }
 }
